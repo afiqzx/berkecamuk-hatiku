8076b0670a046a3a576258d81aa6c8833b19dd36
Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>
2023-07-05 21:17:09 +0900

    REEEEEEEEEEEEEEEEEEEEEEEE
    Signed-off-by: Mohammad Afiq <46773936+afiqzx@users.noreply.github.com>

---
+++ src/Vec2.cpp
@@ -0,0 +1,86 @@
+#include "Vec2.hpp"
+#include <functional>
+#include <cmath>
+#include <math.h>
+
+
+Vec2::Vec2(){
+    m_x = 0.0f;
+    m_y = 0.0f;
+}
+
+Vec2::Vec2(float x, float y){
+    m_x = x;
+    m_y = y;
+}
+
+
+void Vec2::Add(const Vec2& v){
+    m_x += v.m_x;
+    m_y += v.m_y;
+}
+
+void Vec2::Sub(const Vec2& v){
+    m_x -= v.m_x;
+    m_y -= v.m_y;
+}
+
+void Vec2::Scale(const float n){
+    m_x *= n;
+    m_y *= n;
+}
+
+Vec2 Vec2::Rotate(const float angle) const {
+    Vec2 ret(0.0f, 0.0f);
+    
+    ret.m_x = m_x * cos(angle) - m_y * sin(angle);
+    ret.m_y = m_x * sin(angle) + m_y * sin(angle);
+
+    return ret;
+}
+
+float Vec2::Magnitude() const {
+    return sqrt(m_x * m_x + m_y * m_y);
+}
+
+float Vec2::MagnitudeSquared() const {
+    return m_x * m_x + m_y * m_y;
+}
+
+Vec2& Vec2::Normalize(){
+    float magnitude = Magnitude();
+
+    m_x /= magnitude;
+    m_y /= magnitude;
+
+    return *this;
+}
+
+Vec2 Vec2::UnitVector() const {
+    Vec2 ret(m_x, m_y);
+
+    ret.Normalize();
+
+    return ret;
+}
+
+Vec2 Vec2::Normal() const {
+    Vec2 ret(m_x, m_y);
+
+    ret.Normalize();
+
+    return ret;
+}
+
+float Vec2::Dot(const Vec2& v) const {
+    float ret;
+    ret = m_x * v.m_x + m_y * v.m_y;
+
+    return ret;
+}
+
+float Vec2::Cross(const Vec2& v) const {
+    return 0.0f;
+}
+
+
---
+++ src/Vec2.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+struct Vec2 {
+    float m_x;
+    float m_y;
+
+    Vec2();
+    Vec2(float x, float y);
+
+    ~Vec2() = default;
+
+    void Add(const Vec2& v);
+    void Sub(const Vec2& v);
+    void Scale(const float n);
+    Vec2 Rotate(const float angle) const;
+
+    float Magnitude() const;
+    float MagnitudeSquared() const;
+
+    Vec2& Normalize();
+    Vec2 UnitVector() const;
+    Vec2 Normal() const;
+
+    float Dot(const Vec2& v) const;
+    float Cross(const Vec2& v) const;
+
+};
+
